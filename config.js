3627523d070f6bdcd1a91b6398813e30c6361d80
mwvdev <mwvdev@users.noreply.github.com>
2017-02-17 18:17:43 +0100

    Parameterized script to allow for easier submission of multiple domains.

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 module.exports = {
     url: 'https://ssl.gratisdns.dk/ddns.phtml',
-    externalIPLookupUrl: 'https://diagnostic.opendns.com/myip',
-
-    // Fill in your GratisDNS information below:
-    user: '',
-    password: '',
-    domain: '',
-    host: ''
+    externalIPLookupUrl: 'https://diagnostic.opendns.com/myip'
 };
--- gratisdns.js
+++ gratisdns.js
@@ -1,25 +1,36 @@
+if(process.argv.length < 5) {
+    console.error('Usage: gratisdns <username> <password> <domain> <hostname>');
+    process.exit(1);
+}
+
 var request = require('request');
 var validator = require('validator');
-
 var config = require('./config');
 
+var username = process.argv[2];
+var password = process.argv[3];
+var domain = process.argv[4];
+var hostname = process.argv[5];
+
 function updateDDNS(ip, callback) {
-    var queryString = {u: config.user, p: config.password, d: config.domain, h: config.host, ip: ip};
+    var queryString = {u: username, p: password, d: domain, h: hostname, ip: ip};
     request({url: config.url, qs: queryString}, function(error, response, body) {
         if(error) {
             return callback(error);
         }
 
-        return callback(null, body);
+        return callback(null, domain, hostname, body);
     });
 }
 
-function updateDDNSCallback(error, body) {
+function updateDDNSCallback(error, domain, hostname, body) {
     if(error) {
-        console.log('An error occurred while submitting DDNS update to GratisDNS: ' + error);
+        console.log('Failed submitting DDNS update to GratisDNS for domain: %s and host: %s. Received response: %s',
+            domain, hostname, body);
     }
     else {
-        console.log('Submitted a DDNS update to GratisDNS successfully! Received response: ' + body);
+        console.log('Successfully submitted a DDNS update to GratisDNS for domain: %s and host: %s. Received response: %s',
+            domain, hostname, body);
     }
 }
 
@@ -43,6 +54,7 @@ var lookupCallback = function(error, ip) {
     }
 
     console.log('Found external IP: ' + ip);
+
     updateDDNS(ip, updateDDNSCallback);
 };
 
